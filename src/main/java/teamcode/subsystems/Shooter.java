515dc1cc52be3ea5e7ef422e1f19b2e4a7a5badf
Titan Robotics Club <trc492@users.noreply.github.com>
2026-02-28 21:56:16 -0800

    Increased shooter ready timeout time to 5 seconds, so we will notice it's timed out. This allows us to check who's timing out and may need to retune that PID coefficients. Upated trclib.

--- src/main/java/teamcode/subsystems/Shooter.java
+++ src/main/java/teamcode/subsystems/Shooter.java
@@ -112,7 +112,7 @@ public class Shooter extends TrcSubsystem
         public static final double SHOOTER_VEL_TRIGGER_SETTLING = 0.0;
         public static final double SHOOTER_VEL_TRIGGER_TIMEOUT  = 1.0;
         public static final double SHOOTER_RPM_CONFLICT_ZONE_ADJ= 0.0;
-        public static final double SHOOTER_READY_TIMEOUT        = 2.0;          // in sec
+        public static final double SHOOTER_READY_TIMEOUT        = 5.0;          // in sec
         // Left Shooter Motor Characteristics
         public static final String LSHOOTER_PRIMARY_MOTOR_NAME  = SUBSYSTEM_NAME + ".LeftPrimaryMotor";
         public static final boolean LSHOOTER_PRIMARY_MOTOR_INVERTED = false;
